23e79821a5e8838d8e8abd3e3209f571139de84b
cristianmarce6 <cristianmarce6@gmail.com>
2019-02-20 01:23:11 -0500

    archivos faltantes

---
+++ application/views/footer.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Footer
+ * Vista que carga la informacion de pie de páginan con scripts js.
+ */
+?>
+		<script src="<?= base_url('/assets/js/main.js'); ?>"></script>
+    </body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * Header
+ * vista que carga la parte de estilos e información meta de la página.
+ */
+?>
+<!DOCTYPE HTML>
+<!--
+    Eventually by HTML5 UP
+    html5up.net | @ajlkn
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+    <head>
+        <title>Agenda web</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="<?= base_url('assets/css/custom.css'); ?>" />
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+        <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+        <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    </head>
+    <body>
